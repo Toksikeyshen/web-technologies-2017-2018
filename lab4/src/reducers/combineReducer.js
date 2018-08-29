55d5f584f441ca94b239142999dffe790f33d8f5
Egor Yaroshuk <36363805+Toksikeyshen@users.noreply.github.com>
2018-08-30 01:08:45 +0300

    Delete

--- lab4/src/reducers/combineReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import userData from './userData'
-import userFollowersAndRepos from './userFollowersAndRepos'
-
-export default combineReducers({userData, info: userFollowersAndRepos})
